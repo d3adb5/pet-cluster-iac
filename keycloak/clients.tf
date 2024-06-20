75bafae039713bd4949489cb2cef8edeba88839b
d3adb5 <me@d3adb5.net>
2024-06-19 19:28:41 -0700

    feat: add default and optional client scopes for argocd
    Add default and optional client scopes for the Argo CD OIDC client.

--- keycloak/clients.tf
+++ keycloak/clients.tf
@@ -33,3 +33,30 @@ resource "keycloak_openid_client" "argocd" {
     "https://argo.d3adb5.net",
   ]
 }
+
+resource "keycloak_openid_client_default_scopes" "argocd" {
+  realm_id  = keycloak_realm.laboratory.id
+  client_id = keycloak_openid_client.argocd.id
+
+  default_scopes = [
+    "acr",
+    "basic",
+    "email",
+    "profile",
+    "roles",
+    "web-origins",
+    keycloak_openid_client_scope.groups.name,
+  ]
+}
+
+resource "keycloak_openid_client_optional_scopes" "argocd" {
+  realm_id  = keycloak_realm.laboratory.id
+  client_id = keycloak_openid_client.argocd.id
+
+  optional_scopes = [
+    "address",
+    "microprofile-jwt",
+    "offline_access",
+    "phone",
+  ]
+}
